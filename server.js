c3dad79646f2654e37815572070a384be426855b
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-05 16:17:42 +0100

    made a correction

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const carRouter = require('./cars/carsRouter');
+const carsRouter = require('./cars/carsRouter');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-// server.use('/api/cars', carsRouter);
+server.use('/api/cars', carsRouter);
 
 server.get("/", (req, res) => {
     res.send("<h3>DB Helpers with knex</h3>");
